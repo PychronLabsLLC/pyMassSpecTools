6a946cf71b6e0d0e699f2ab9fda7f66d4b5b38e2
Jake Ross <jirhiker@gmail.com>
2021-12-10 14:54:43 -0700

    initial commit

---
+++ mst/loggable.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+# ===============================================================================
+# Copyright 2011 Jake Ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+# ============= standard library imports ========================
+from threading import current_thread
+
+# ============= enthought library imports =======================
+from traits.api import Any, String
+
+# ============= local library imports  ==========================
+from mst.base_fs import BaseFS
+from mst.core.helpers.color_generators import colorname_generator
+from mst.core.helpers.logger_setup import new_logger
+
+color_name_gen = colorname_generator()
+NAME_WIDTH = 40
+__gloggers__ = dict()
+
+
+class unique(object):
+    def __init__(self):
+        self._registry = {}
+
+    def __call__(self, f):
+        def wrapped_f(*args, **kw):
+            obj = args[0]
+            msg = args[1]
+            hmsg = hash(msg)
+            ido = id(obj)
+            if ido not in self._registry:
+                self._registry[ido] = []
+
+            msgs = self._registry[ido]
+            if hmsg not in msgs:
+                msgs.append(hmsg)
+                f(*args)
+
+        return wrapped_f
+
+
+class Loggable(BaseFS):
+    """ """
+
+    application = Any
+    logger = None  # (transient=True)
+    name = String
+    logger_name = String
+    # use_logger_display = True
+    # use_warning_display = True
+    logcolor = "black"
+    shared_logger = False
+
+    def __init__(self, *args, **kw):
+        super(Loggable, self).__init__(*args, **kw)
+        self.init_logger()
+
+    def init_logger(self):
+        t = str(type(self))
+        if self.shared_logger and t in __gloggers__:
+            self.logger = __gloggers__[t]
+        else:
+            self._add_logger()
+
+    def report_logger_stats(self):
+        self.debug("&&&& len __gloggers__ = {}".format(len(__gloggers__)))
+
+    @unique()
+    def unique_warning(self, *args, **kw):
+        self.warning(*args, **kw)
+
+    @unique()
+    def unique_info(self, *args, **kw):
+        self.info(*args, **kw)
+
+    @unique()
+    def unique_debug(self, *args, **kw):
+        self.debug(*args, **kw)
+
+    def warning(self, msg):
+        """ """
+
+        if self.logger is not None:
+            self._log_("warning", msg)
+
+    def info(self, msg, dolater=False, color=None):
+        """ """
+        if self.logger is not None:
+            self._log_("info", msg)
+
+    def debug_exception(self):
+        import traceback
+
+        exc = traceback.format_exc()
+        self.debug(exc)
+        return exc
+
+    def warning_exception(self):
+        import traceback
+
+        exc = traceback.format_exc()
+        self.warning_dialog(exc)
+        return exc
+
+    def critical(self, msg):
+        self._log_("critical", msg)
+
+    def debug(self, msg):
+        self._log_("debug", msg)
+
+    def log(self, msg, level=10):
+        def log(m, *args, **kw):
+            self.logger.debug(m)
+            self.logger.log(level, m, *args, **kw)
+
+        self._log_(log, msg)
+
+    # dialogs
+    def message(self, msg):
+        self.info(msg)
+
+    # private
+    def _add_logger(self):
+        """ """
+        if self.logger_name:
+            name = self.logger_name
+        elif self.name:
+            name = self.name
+        else:
+            name = self.__class__.__name__
+
+        if self.logger is None:
+            self.logger = new_logger(name)
+            __gloggers__[name] = self.logger
+
+        c = next(color_name_gen)
+        if c in ["gray", "silver", "greenyellow"]:
+            c = next(color_name_gen)
+        self.logcolor = c
+
+    def _log_(self, func, msg):
+
+        # def get_thread_name():
+        #     name = 'foo'
+        #     # ct = current_thread()
+        #     # name = ct.name
+        #     from pychron.core.ui.thread import currentThreadName
+        #     # if name.startswith('Dummy'):
+        #     #     name = currentThreadName()
+        #
+        #     return name
+
+        if self.logger is None:
+            return
+
+        # extras = {'threadName_': get_thread_name()}
+        if isinstance(func, str):
+            func = getattr(self.logger, func)
+
+        if isinstance(msg, (list, tuple)):
+            msg = ",".join(map(str, msg))
+
+        msg = self._post_process_msg(msg)
+        # func(msg, extra=extras)
+        func(msg)
+
+    def _post_process_msg(self, msg):
+        return msg
+
+    # handlers
+    def _name_changed(self):
+        self._add_logger()
+
+    def _logger_name_changed(self):
+        self._add_logger()
+
+
+# class Loggable(HasTraits, Loggable):
+#     def __init__(self, *args, **kw):
+#         super().__init__(*args, **kw)
+#         self.init_logger()
+
+# ============= EOF =============================================
---
+++ mst/mass_spec/mass_spec_recaller.py
@@ -0,0 +1,102 @@
+# ===============================================================================
+# Copyright 2013 Jake Ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+# ============= enthought library imports =======================
+from __future__ import absolute_import
+
+from traits.api import Instance
+
+from pychron.loggable import Loggable
+from mst.mass_spec.database.massspec_database_adapter import MassSpecDatabaseAdapter
+from mst.mass_spec.mass_spec_analysis import MassSpecAnalysis, MassSpecBlank
+from mst.constants import IRRADIATION_KEYS
+
+
+class MassSpecRecaller(Loggable):
+    db = Instance(MassSpecDatabaseAdapter)
+
+    def is_connected(self):
+        return self.db.connected
+
+    def connect(self):
+        return self.db.connect()
+
+    def get_flux(self, identifier):
+        db = self.db
+        with db.session_ctx():
+            return db.get_flux(identifier)
+
+    def get_production(self, irrad, level):
+        db = self.db
+        prod = {}
+        with db.session_ctx():
+            irrad = db.get_irradiation_level(irrad, level)
+            dbprod = irrad.production
+            name = dbprod.Label
+
+            prod["Ca_K"] = [dbprod.CaOverKMultiplier, dbprod.CaOverKMultiplierEr]
+            prod["Cl_K"] = [dbprod.ClOverKMultiplier, dbprod.ClOverKMultiplierEr]
+
+            for k, _ in IRRADIATION_KEYS:
+                k = k.capitalize()
+                prod[k] = [getattr(dbprod, k), getattr(dbprod, "{}Er".format(k))]
+            prod["name"] = name
+
+        return prod
+
+    def find_analysis(self, labnumber, aliquot, step):
+
+        db = self.db
+        with db.session_ctx():
+            dbrec = db.get_analysis(labnumber, aliquot, step)
+            if dbrec:
+                # need to handle blanks differently
+                # labnumber in mass spec for blanks is -1
+                if labnumber == -1:
+                    klass = MassSpecBlank
+                else:
+                    klass = MassSpecAnalysis
+
+                rec = klass()
+                rec.sync(dbrec)
+                irradpos = db.get_irradiation_position(dbrec.IrradPosition)
+                r = irradpos.IrradiationLevel
+                n, l = r[:-1], r[-1:]
+
+                dbirrad = db.get_irradiation_level(n, l)
+
+                rec.sync_irradiation(dbirrad)
+                for iso in dbrec.isotopes:
+                    det = iso.detector
+                    c = db.get_baseline_changeable_item(iso.baseline.BslnID)
+                    rec.sync_baselines(det.detector_type.Label, c.InfoBlob, c.PDPBlob)
+
+                    c = db.get_pdp(iso.IsotopeID)
+                    if c:
+                        rec.sync_fn(iso.Label, c.PDPBlob)
+
+                    # prefs = db.get_latest_preferences(iso.IsotopeID, iso.Label)
+
+                    # riso = rec.isotopes[iso.Label]
+                    # rec.sync_filtering(riso, prefs)
+
+                    # prefs = db.get_latest_baseline_preferences(iso.baseline.BslnID)
+                    # rec.sync_filtering(riso.baseline, prefs)
+
+                return rec
+
+
+# ============= EOF =============================================
